0735b356d5d589947c13bbe4c7c8ba98ad25e9fc
David <111059922+denisjam400@users.noreply.github.com>
2024-05-02 15:05:48 +0100

    Update and rename StarsCanvas.tsx to StarsCanvas.jsx

--- components/Canvas/StarsCanvas.jsx
+++ components/Canvas/StarsCanvas.jsx
@@ -4,7 +4,7 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
-const Stars = (props: any) => {
+const Stars = (props) => {
     const ref = useRef();
     const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
 
